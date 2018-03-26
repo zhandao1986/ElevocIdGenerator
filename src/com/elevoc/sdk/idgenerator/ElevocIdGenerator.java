34636be42e56d17837979d2854b96391c5d93b83
HUPENG <hupeng@imudges.com>
2018-03-26 11:21:25 +0800

    update

--- src/com/elevoc/sdk/idgenerator/ElevocIdGenerator.java
+++ src/com/elevoc/sdk/idgenerator/ElevocIdGenerator.java
@@ -20,5 +20,47 @@ public class ElevocIdGenerator {
     }
 
 
+    public static String uuid8(){
+        try {
+            return String.valueOf(_IdWorker.getFlowIdWorkerInstance().nextId()).substring(0,8);
+        }catch (Exception e){
+            Random random = new Random(System.currentTimeMillis());
+            String tmp = "";
+            for (int i=0; i<8; i++){
+                tmp += random.nextInt(10);
+            }
+            return tmp;
+        }
+    }
 
+
+    public static String uuid16(){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+        try {
+            return simpleDateFormat.format(new Date(System.currentTimeMillis())).substring(2,8) + String.valueOf(_IdWorker.getFlowIdWorkerInstance().nextId()).substring(0,10);
+        }catch (Exception e){
+            Random random = new Random(System.currentTimeMillis());
+            String tmp = "";
+            for (int i=0; i<16; i++){
+                tmp += random.nextInt(10);
+            }
+            return simpleDateFormat.format(new Date(System.currentTimeMillis())).substring(2,8)  + tmp.substring(0,10);
+        }
+    }
+
+
+    public static String uuid64(){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+        try {
+            String tmp =  simpleDateFormat.format(new Date(System.currentTimeMillis())) + _IdWorker.getFlowIdWorkerInstance().nextId() + _IdWorker.getFlowIdWorkerInstance().nextId() + _IdWorker.getFlowIdWorkerInstance().nextId();
+            return tmp.substring(0,100);
+        }catch (Exception e){
+            Random random = new Random(System.currentTimeMillis());
+            String tmp = "";
+            for (int i=0; i<47; i++){
+                tmp += random.nextInt(10);
+            }
+            return simpleDateFormat.format(new Date(System.currentTimeMillis())) + tmp;
+        }
+    }
 }
--- src/com/elevoc/sdk/idgenerator/Main.java
+++ src/com/elevoc/sdk/idgenerator/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String id = ElevocIdGenerator.uuid32();
+        String id = ElevocIdGenerator.uuid64();
         System.out.println(id);
+        System.out.println(id.length());
     }
 }
